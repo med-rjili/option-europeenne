59104aa2244ccf9869ee45842f0158449e963933
fawzeus <rjilifawzi@gmail.com>
2024-02-08 04:05:06 +0100

    fix bugs

--- option.cpp
+++ option.cpp
@@ -3,9 +3,9 @@
 //                       class matrice
 //--------------------------------------------------------------------------
 matrice::matrice(vector<pair <int,int>> indexes,Vecteur<double> valeurs,int dim):indexes(indexes),valeurs(valeurs),dim(dim){}
-Vecteur<double>& matrice::matix_vector(const Vecteur<double>& v)
+Vecteur<double> matrice::matrix_vector(const Vecteur<double>& v)
 {
-    Vecteur<double> result(indexes.size(),0);
+    Vecteur<double> result(dim,0);
     for (int i=0;i<dim;i++)
     {
         for(int j=0;j<indexes.size();j++)
@@ -26,3 +26,22 @@ int matrice::val(int i,int j)const
       }
       return 0;
     }
+
+void matrice::print() const {
+  map <pair<int,int>,double> mp;
+  bool exist[dim][dim] = {{false}};
+  for(int i=0;i<indexes.size();i++){
+    exist[indexes[i].first][indexes[i].second]=true;
+    mp[indexes[i]]=valeurs[i];
+  }
+  for(int i=0;i<dim;i++){
+    cout<<"(";
+    for(int j=0;j<dim-1;j++){
+      if (exist[i][j]) cout<< mp[pair<int,int> (i,j)]<<" ";
+      else cout<<0<<" ";
+    }
+    if (exist[i][dim-1]) cout<< mp[pair<int,int> (i,dim-1)]<<")\n";
+    else cout<<"0)\n";
+  }
+
+}
